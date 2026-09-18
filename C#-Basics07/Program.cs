598e7b7bf6d1df52966fa03d1f52b8df61dd40f7
menna <abcda876543@gmail.com>
2026-09-19 00:17:45 +0300

    Add project files.

---
+++ C#-Basics07/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__Basics07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
